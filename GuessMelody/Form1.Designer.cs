584af346255893f40009de4ca49cb201b15d54d1
cradle.brn <cradle.brn@gmail.com>
2018-07-26 15:06:28 +0300

    Добавьте файлы проекта.

---
+++ GuessMelody/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GuessMelody
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 299);
+            this.Name = "fMain";
+            this.Text = "Угадай мелодию";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
