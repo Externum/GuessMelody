5f8447e5f6ee9552a3fdf844f49683a7d24de011
cradle.brn <cradle.brn@gmail.com>
2018-08-02 12:46:12 +0300

    2

---
+++ GuessMelody/fMessage.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace GuessMelody
+{
+    partial class fMessage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblShowAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblMessage.Location = new System.Drawing.Point(115, 22);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(124, 42);
+            this.lblMessage.TabIndex = 0;
+            this.lblMessage.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(22, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Да";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(189, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Нет";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTime.Location = new System.Drawing.Point(152, 82);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(51, 55);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "0";
+            // 
+            // lblShowAnswer
+            // 
+            this.lblShowAnswer.AutoSize = true;
+            this.lblShowAnswer.Location = new System.Drawing.Point(22, 151);
+            this.lblShowAnswer.Name = "lblShowAnswer";
+            this.lblShowAnswer.Size = new System.Drawing.Size(87, 13);
+            this.lblShowAnswer.TabIndex = 4;
+            this.lblShowAnswer.Text = "Показать ответ";
+            this.lblShowAnswer.Click += new System.EventHandler(this.lblShowAnswer_Click);
+            // 
+            // fMessage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 249);
+            this.Controls.Add(this.lblShowAnswer);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "fMessage";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.fMessage_FormClosing);
+            this.Load += new System.EventHandler(this.fMessage_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblShowAnswer;
+    }
+}
---
+++ GuessMelody/fParam.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GuessMelody
+{
+    public partial class fParam : Form
+    {
+        public fParam()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Victorina.allDirectories = cbAllDirectory.Checked;
+            Victorina.gameDuration = Convert.ToInt32(cbGameDuration.Text);
+            Victorina.musicDuration = Convert.ToInt32(cbMusicDuration.Text);
+            Victorina.randomStart = cbRandomStart.Checked;
+            Victorina.WriteParam();
+            this.Hide();
+        }
+
+        void Set()
+        {
+            cbAllDirectory.Checked = Victorina.allDirectories;
+            cbGameDuration.Text = Victorina.gameDuration.ToString();
+            cbMusicDuration.Text = Victorina.musicDuration.ToString();
+            cbRandomStart.Checked = Victorina.randomStart;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Set();
+            this.Hide();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string[] musicList = Directory.GetFiles(fbd.SelectedPath, "*.mp3", cbAllDirectory.Checked?SearchOption.AllDirectories:SearchOption.TopDirectoryOnly);
+                Victorina.lastFolder = fbd.SelectedPath;
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(musicList);
+                Victorina.list.Clear();
+                Victorina.list.AddRange(musicList);
+            }
+        }
+
+        private void fParam_Load(object sender, EventArgs e)
+        {
+            Set();
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(Victorina.list.ToArray());
+        }
+    }
+}
